054bb9e2082214b508e364b33f79fa7e4403c5d7
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2019-12-29 10:39:03 +0300

    Maha lab

--- app/main.js
+++ app/main.js
@@ -1,1 +1,18 @@
-console.log('testing')
+// lab -> Maha
+// Starting functionconst isEven = function (num) {  return num % 2 === 0}
+// console.log('Is 1 even?', isEven(1))console.log('Is 2 even?', isEven(2))
+// 1. Rewrite the isEven function using arrow syntax.// LAB CODE GOES HERE
+const isEven = (num) => {
+    return num % 2 === 0;
+}
+
+// 2. Rewrite the isEven function using arrow syntax with implicit return.// LAB CODE GOES HERE
+const isEven = (num) => num % 2 === 0;
+
+// 3. Rewrite the isEven function using arrow syntax with implicit return and single parameter no paranthesis.// LAB CODE GOES HERE
+const isEven = num => num % 2 === 0;
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
